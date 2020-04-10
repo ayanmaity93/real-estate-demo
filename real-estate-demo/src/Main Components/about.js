49af656d8616c20d643a28561f6a1480953c5d52
Ayan Maity <ayan.maity@live.com>
2020-04-11 03:37:29 +0530

    Modified Property Page

--- real-estate-demo/src/Main Components/about.js
+++ real-estate-demo/src/Main Components/about.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import Testimonials from './testimonials.js';
 
 
 function About () {
 
-  const [isHome,setHome] = useState(false);
-  function toHome(){
-    setHome(true);
-  }
-
-  if(isHome){
     return(
-      <Redirect to="/" />
-    )
-  } else {
-    return(
-      <div>
-        <h1>About Page</h1>
-        <a onClick={toHome}>Click Here to Home Page</a>
-      </div>
+      <>
+        <Testimonials></Testimonials>
+      </>
     ) 
-  }
 }
 
 export default About;
